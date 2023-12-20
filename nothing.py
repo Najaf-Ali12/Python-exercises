ff1ecc7c28ecc468beb015d74c24d13a5f841af2
Najaf Ali Alkhani <150806918+Najaf-Ali12@users.noreply.github.com>
2023-12-19 17:32:40 -0800

    Add files via upload

---
+++ nothing.py
@@ -0,0 +1,15 @@
+#a,b="red","blue"
+#a,b=b,a
+#print(a,b)
+#str=("hello world!")
+#print(str)
+#print(str*2)
+a=1.0
+b="1"
+c="1.1"
+print(a+float(b))
+print(float(b)+float(c))
+#print(a+int(c))
+print(a+int(float(c)))
+#print(int(a)+int(float(c))))
+print(2.0*b)
