e811bb4744cc2c316bbd883731aa79aee01ef5d5
Najaf Ali Alkhani <150806918+Najaf-Ali12@users.noreply.github.com>
2024-01-09 18:11:06 +0500

    Add files via upload

---
+++ factorial using function and for loop.py
@@ -0,0 +1,10 @@
+#Develop a Python function that calculates the factorial of a given number.
+#Test the function by finding the factorial of 5.
+def fact():
+        factorial=1
+        for i in range(1,n+1):
+                factorial=factorial*i
+        print("the factorial of",n,"is",factorial)
+n=int(input("enter the number"))
+fact()
+
