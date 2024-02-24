bc82b5907c96c0613cfc12348ec7bed654f614db
Najaf Ali Alkhani <150806918+Najaf-Ali12@users.noreply.github.com>
2024-02-24 10:54:46 +0500

    Update decimal to binary converator.py

--- decimal to binary converator.py
+++ decimal to binary converator.py
@@ -1,11 +1,13 @@
 num=int(input("enter a number"))
-x=[]
+given_number=num #this is for doing string catenation in last line
+binarydigits_store=[] #will store remainder
 while num//2 >=1:
-    y=num%2
-    x.append(y)
+    remainder_store=num%2  #will takes remainder
+    binarydigits_store.append(remainder_store)
     num=num//2
-x.append(1)
-x.reverse()
-print(x)
+binarydigits_store.append(1)
+binarydigits_store.reverse()
+binarynumbers=''.join(map(str,binarydigits_store)) #will change list in string join all elements and store them in binarynumbers
+print("the binary number for",given_number,"is",binarynumbers)
     
     
