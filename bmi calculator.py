1937151b7e4e1d1c9ae31752aae12aebadff138a
Najaf Ali Alkhani <150806918+Najaf-Ali12@users.noreply.github.com>
2024-03-11 14:15:35 +0500

    Add files via upload

---
+++ bmi calculator.py
@@ -0,0 +1,5 @@
+#you have to find BMI , the output will not be in float
+#BMI=weight/height**2
+weight=int(input("enter your weight in kg"))
+height=float(input("enter your height in meters"))
+print("the bmi is",round(weight/height**2))
