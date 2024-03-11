012db80fa28efe4327cf96f01ea43491808448e2
Najaf Ali Alkhani <150806918+Najaf-Ali12@users.noreply.github.com>
2024-03-11 14:06:51 +0500

    Add files via upload

---
+++ life days teller.py
@@ -0,0 +1,7 @@
+#write a programm to find how many days,weeks,
+#months and years we have left if we live untill 90 years old.
+age=int(input("enter your age"))
+print("you have left",90-age," years behind")
+print("you have left",(90-age)*12," months behind")
+print("you have left",(90-age)*32," weeks behind")
+print("you have left",(90-age)*365," days behind")
